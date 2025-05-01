a7fe5751b50c6e1387b3df30e62044ef5f1fbc56
briana892 <bri232308@gmail.com>
2025-05-01 16:47:00 -0400

    upload project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+ return (
+  <div classname="container">
+ <div classname="nav-bar"></div>
+ <button>Apps</button>
+ <button>Users</button>
+ <div classname="body"></div>
+ <div classname="footer"></div>
+ </div>
+  );
+}
+
+export default App;
