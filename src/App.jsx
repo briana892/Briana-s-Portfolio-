13582cd4ecb9d9b41a0176310f1d3553cca983ab
briana892 <bri232308@gmail.com>
2025-05-07 17:36:25 -0400

    i added sections and boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import React from "react";
+import "./index.css";
+
 function App() {
-  return ()
+  return (
+    <div className="flex-container">
+      <h1>Who is Briana Ailyn Ramos?</h1>
+      <h2>bri232308@gmail.com</h2>
+      <h3>About Me</h3>
+      <div className="container">
+        {" "}
+        My name is Briana A. Ramos, and I am 16 years old, born on December 23,
+        2008. Currently, I am in the 12th grade and will be graduating a year
+        early. I made the decision to graduate early to gain a head start on my
+        aspiration of becoming a clinical psychologist.
+      </div>
+      <h4>My Goals And Dreams..</h4>
+      <div className="ok-container">
+        Upon completing high school, I intend to pursue a degree in Psychology
+        in college. The college I will be attending is NYIT. After I finish all
+        years of school needed, I will relocate from New York, and ultimately
+        aspire to become a Clinical Psychologist.
+      </div>
+      <h5>Resume</h5>
+      <div className="yes-container">
+        {" "}
+        Experience Summer Youth Employment Program 2024 Responsibilities:
+        Created health and wellness presentation Babysitting Responsibilities:
+        Include working for children ages 2,4,7, and 8. El puente, March 2025-
+        May 2025 Responsiblities: Digital Stuartship/ Scholars industry Program
+      </div>
+    </div>
+  );
 }
-export default 
-. flex containter {
-  display: flex;
-  flex-direction: column
-}
+
+export default App;
